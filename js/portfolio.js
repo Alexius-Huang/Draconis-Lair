09683c36040eea9aedacceada1f11af50d51d64a
Maxwell-Alexius <destinydragon0614@yahoo.com.tw>
2017-02-23 00:31:07 +0800

    New version

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,6 @@
-var $portfolios = ['simple-torus-knot', 'doppler-effect', 'snake', 'brick-breaker', 'tetris', 'simple-harmonic-motion', 'game-of-life', '2048'];
-var $platforms  = ['draconis', 'github', 'codepen', 'medium'];
-
 $(document).ready(function() {
+  var $portfolios = ['event-horizon', 'simple-torus-knot', 'doppler-effect', 'snake', 'brick-breaker', 'tetris', 'simple-harmonic-motion', 'game-of-life', '2048'];
+  var $platforms  = ['draconis', 'github', 'codepen', 'medium'];
   resizeImgWidth();
   resizePortfolioSection();
   $(window).resize(function() {
@@ -17,33 +16,33 @@ $(document).ready(function() {
         $(this).attr('src', './logos/lightblue_' + platform + '_logo.png');
       });
     });
-  })
-});
+  });
 
-function resizeImgWidth() {
-  $portfolios.forEach(function(portfolio) {
-    $platforms.forEach(function(platform) {
-      $('#' + platform + '-' + portfolio + '-logo').css('height', $('#header-' + portfolio).outerHeight());
-    });
-    $('#img-' + portfolio).css({
-      width: $('#img-container-' + portfolio).width(),
-      maxWidth: $('#img-container-' + portfolio).width()
-    });
-  });  
-}
-
-function resizePortfolioSection() {
-  if ($(window).width() <= 768) {
+  function resizeImgWidth() {
     $portfolios.forEach(function(portfolio) {
-      $('#paragraph-' + portfolio).css({
-        height: $('#paragraph-' + portfolio).outerHeight(true)
+      $platforms.forEach(function(platform) {
+        $('#' + platform + '-' + portfolio + '-logo').css('height', $('#header-' + portfolio).outerHeight());
       });
-    });
-  } else {
-    $portfolios.forEach(function(portfolio) {
-      $('#paragraph-' + portfolio).css({
-        height: $('#img-' + portfolio).height()
-      })
-    });
+      $('#img-' + portfolio).css({
+        width: $('#img-container-' + portfolio).width(),
+        maxWidth: $('#img-container-' + portfolio).width()
+      });
+    });  
   }
-}
+
+  function resizePortfolioSection() {
+    if ($(window).width() <= 768) {
+      $portfolios.forEach(function(portfolio) {
+        $('#paragraph-' + portfolio).css({
+          height: $('#paragraph-' + portfolio).outerHeight(true)
+        });
+      });
+    } else {
+      $portfolios.forEach(function(portfolio) {
+        $('#paragraph-' + portfolio).css({
+          height: $('#img-' + portfolio).height()
+        })
+      });
+    }
+  }
+});
